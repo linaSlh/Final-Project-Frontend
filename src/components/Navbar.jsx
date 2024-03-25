afb8ac4ce4f2fd4d6033ebd58f48255ad2081c84
linaSlh <lyna.slh@gmail.com>
2024-03-25 16:04:40 +0100

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,32 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";                     // <== IMPORT 
+import { AuthContext } from "../context/auth.context"; 
 
 function Navbar() {
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext); 
+
   return (
     <nav>
       <Link to="/">
         <button>Home</button>
       </Link>
 
-      <Link to="/projects">
-        <button>Projects</button>
-      </Link>
+      {isLoggedIn && (
+        <>
+          <span>Welcome {user.name}</span>
+          <Link to="/projects">
+            <button>Projects</button>
+          </Link>        
+          <button onClick={logOutUser}>Logout</button>
+        </>
+      )}
+
+        {!isLoggedIn && (
+              <>
+                <Link to="/signup"> <button>Sign Up</button> </Link>
+                <Link to="/login"> <button>Login</button> </Link>
+              </>
+            )}
     </nav>
   );
 }

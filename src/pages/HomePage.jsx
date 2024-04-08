e300a7c40369d39d101dc66c9fba4c9fa1e51a5a
linaSlh <lyna.slh@gmail.com>
2024-04-08 23:07:56 +0200

    homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,11 +4,11 @@ function HomePage() {
    
       <span><h1>Discover, Wander,</h1></span><span className="highlight"><h1>Connect.</h1></span>
       <h2>Where Adventures Meet Memories</h2>
-      <div className="card">
+      {/* <div className="card">
        <p>
         <code>{(import.meta.env.VITE_SERVER_URL)}</code>
        </p>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/pages/PostListPage.jsx
+++ src/pages/PostListPage.jsx
@@ -1,39 +1,3 @@
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-
-// import PostCard from "../components/PostCard";
-// import AddPost from "../components/AddPost";
-
-// const API_URL = "http://localhost:5005";
-
-// function PostListPage() {
-//   const [posts, setPosts] = useState([]);
-
-//   const getAllPosts = () => {
-//     axios
-//       .get(`${API_URL}/api/posts`)
-//       .then((response) => setPosts(response.data))
-//       .catch((error) => console.log(error));
-//   };
-
-//   // We set this effect will run only once, after the initial render
-//   // by setting the empty dependency array - []
-//   useEffect(() => {
-//     getAllPosts();
-//   }, []);
-
-//   return (
-//     <div className="PostListPage">
-//       <AddPost refreshPosts={getAllPosts} />
-
-//       {posts.map((post) => (
-//         <PostCard key={post._id} {...post} />
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default PostListPage;
 
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -41,7 +5,7 @@ import axios from "axios";
 import PostCard from "../components/PostCard";
 import AddPost from "../components/AddPost";
 import postsService from "../services/posts.service";
-//const API_URL = "http://localhost:5005";
+
 const API_URL = import.meta.env.VITE_SERVER_URL;
 
 function PostListPage() {

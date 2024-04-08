97fc4ef4c15a192c9a55c6971af4537fd743e0c3
linaSlh <lyna.slh@gmail.com>
2024-04-09 01:37:36 +0200

    redo author display

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 
 // We are deconstructing props object directly in the parentheses of the function
 function PostCard ( { title, content, _id , author } ) {
-  
+  const authorName = author ? author.username : '';
   return (
     <div className="PostCard">
       <Link to={`/posts/${_id}`}>
         <h3>{title}</h3>
       </Link>
       <p style={{ maxWidth: "400px" }}>{content} </p>
-      <p>by : {author}</p>
+      <p>by : {authorName}</p>
     </div>
   );
 }

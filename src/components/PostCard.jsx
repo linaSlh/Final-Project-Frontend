dedd3bd90e8d20044ffcd6dd5800bf9f393f509b
linaSlh <lyna.slh@gmail.com>
2024-04-09 00:46:33 +0200

    author display

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,15 +1,33 @@
+
+
 import { Link } from "react-router-dom";
+//added//
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 // We are deconstructing props object directly in the parentheses of the function
 function PostCard ( { title, content, _id , author } ) {
-  
+const [authorName, setAuthorName] = useState("");
+useEffect(() => {
+  const fetchAuthor = async () => {
+    try {
+      const response = await axios.get(`/api/users/${author}`);
+      setAuthorName(response.data.username);
+    } catch (error) {
+      console.error("Error fetching author:", error);
+    }
+  };
+
+  fetchAuthor();
+}, [author]);
+ //end//
   return (
     <div className="PostCard">
       <Link to={`/posts/${_id}`}>
         <h3>{title}</h3>
       </Link>
       <p style={{ maxWidth: "400px" }}>{content} </p>
-      <p>by : {author.username}</p>
+      <p>by : {author}</p>
     </div>
   );
 }

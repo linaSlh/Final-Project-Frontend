43f724a4f4906d050cd40590dd3aea30bdfacc68
linaSlh <lyna.slh@gmail.com>
2024-04-10 00:04:07 +0200

    css add

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,52 @@
+// function HomePage() {
+//   return (
+//     <div className="homepage-container">
+   
+//       <span><h1>Discover, Wander,</h1></span><span className="highlight"><h1>Connect.</h1></span>
+//       <h2>Where Adventures Meet Memories</h2>
+
+//     </div>
+//   );
+// }
+
+// export default HomePage;
 function HomePage() {
   return (
     <div className="homepage-container">
-   
-      <span><h1>Discover, Wander,</h1></span><span className="highlight"><h1>Connect.</h1></span>
+      <h1>Discover, Wander,<span className="highlight">Connect.</span></h1>
       <h2>Where Adventures Meet Memories</h2>
-      {/* <div className="card">
-       <p>
-        <code>{(import.meta.env.VITE_SERVER_URL)}</code>
-       </p>
-      </div> */}
+      
+      <div className="gallery">
+        <div className="gallery-item">
+          <img src="../assets/greece.jpg" alt="greece" />
+          <p>Small text under Image 1</p>
+        </div>
+        <div className="gallery-item">
+          <img src="../assets/machu-picchu.jpg" alt="machu picchu" />
+          <p>Small text under Image 2</p>
+        </div>
+        <div className="gallery-item">
+          <img src="../assets/kimono.jpg" alt="Japan" />
+          <p>Small text under Image 2</p>
+        </div>
+        <div className="gallery-item">
+          <img src="../assets/marrakech.jpg" alt="marrakech" />
+          <p>Small text under Image 2</p>
+        </div>
+        <div className="gallery-item">
+          <img src="../assets/mountains.jpg" alt="mountains" />
+          <p>Small text under Image 2</p>
+        </div>
+        
+      </div>
+      
+      <div className="social-icons">
+        {/* Add social media icons here */}
+      </div>
+      
+      <div className="copyright">
+        {/* Add copyright text here */}
+      </div>
     </div>
   );
 }
